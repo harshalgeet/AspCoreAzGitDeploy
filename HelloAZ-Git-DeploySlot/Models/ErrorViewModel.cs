f1ef951b30f66c6afbf049b28881bdd50c1cdb95
harshalgeet <harshal.geet@gmail.com>
2020-05-08 14:16:59 +0530

    Add project files.

---
+++ HelloAZ-Git-DeploySlot/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloAZ_Git_DeploySlot.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
